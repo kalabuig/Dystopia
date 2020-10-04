66f932a2f7deca1adf7af01b77e21c85edd136f5
Josep Calabuig <72033912+kalabuig@users.noreply.github.com>
2020-10-04 14:26:11 +0200

    Add files via upload
    Escena nova i prefabs nous per a començar a generar ciutat (estructura illa, edifici preparada). Primeres proves de creació d'illes i edificis.

---
+++ Scripts/BuildingSpamer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSpamer : MonoBehaviour
+{
+    public GameObject[] buildings;
+
+    void Start()
+    {
+        int[] rot = new int[4] {0,90,180,270};
+        int randIndex = Random.Range(0, buildings.Length);
+        int randRotIndex = Random.Range(0, 4);
+        GameObject go = Instantiate(buildings[randIndex], transform.position, Quaternion.identity);
+        go.transform.Rotate(0,0,rot[randRotIndex]);
+    }
+
+}
---
+++ Scripts/GameHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    private CameraBehavior cameraBehavior;
+    private Transform playerTransform;
+
+    private float zoom=100f; //Zoom level
+    private float zoomSpeed = 150f; //Zoom in and out speed
+    private float minZoom = 40f; //More close zoom
+    private float maxZoom = 200f; //More far zoom
+    private float zoomWheelSensibility = 20f;
+    
+    private void Awake() {
+        cameraBehavior = Camera.main.GetComponent<CameraBehavior>();
+        playerTransform = GameObject.Find("Player").transform;
+    }
+    
+    void Start()
+    {
+        cameraBehavior.setFocus(() => playerTransform.position);
+        cameraBehavior.setZoom(() => zoom);
+    }
+
+    private void Update() {
+        HandleZoom();
+    }
+
+    private void HandleZoom() {
+        //Keyboard zoom control
+        if(Input.GetKey(KeyCode.KeypadPlus)) { //Zoom in
+            zoom -= zoomSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.KeypadMinus)) { //Zoom out
+            zoom += zoomSpeed * Time.deltaTime;
+        };
+        //Mouse wheel zoom control
+        if(Input.mouseScrollDelta.y > 0) {
+            zoom -= zoomSpeed * Time.deltaTime * zoomWheelSensibility;
+        }
+        if(Input.mouseScrollDelta.y < 0) {
+            zoom += zoomSpeed * Time.deltaTime * zoomWheelSensibility;
+        }
+        //Zoom limits
+        zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+    }
+}
