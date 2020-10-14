2263f29de80e3b5e0da1bfc663e4562c12be9cee
PORTATIL-JOSEP\jllca <jllcalabuig@gmail.com>
2020-10-14 22:05:10 +0200

    no message

---
+++ Dystopia/Assets/Scripts/Crafting/CraftingRecipe.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public struct ItemAmout {
+    public Item item;
+    [Range(1, 9999)]
+    public int amount;
+}
+
+[CreateAssetMenu]
+public class CraftingRecipe : ScriptableObject
+{
+    public List<ItemAmout> materials;
+    public List<ItemAmout> results;
+
+    public bool CanCraft(IItemsContainer itemsContainer) {
+        foreach(ItemAmout itemAmout in materials) {
+            if(itemsContainer.ItemCount(itemAmout.item.ID) < itemAmout.amount) {
+                return false; //there are not enough materials
+            }
+        }
+        return true; //there are enough materials, we can craft it
+    }
+
+    public void Craft(IItemsContainer itemsContainer) {
+        if(CanCraft(itemsContainer)) {
+            //Remove from the container every material used to craft
+            foreach(ItemAmout itemAmout in materials) {
+                for(int i = 0; i < itemAmout.amount; i++) {
+                    Item oldItem = itemsContainer.RemoveItem(itemAmout.item.ID);
+                    oldItem.Destroy();
+                }
+            }
+            //Add to the container every result from the craft
+            foreach(ItemAmout itemAmout in results) {
+                for(int i = 0; i < itemAmout.amount; i++) {
+                    itemsContainer.AddItem(itemAmout.item.GetCopy());
+                    
+                }
+            }
+        }
+    }
+}
---
+++ Dystopia/Assets/Scripts/Crafting/IItemsContainer.cs
@@ -0,0 +1,8 @@
+﻿public interface IItemsContainer
+{
+    int ItemCount(string itemID);
+    Item RemoveItem(string itemID); //returns the reference of the item removed from the container
+    bool RemoveItem(Item item);
+    bool AddItem(Item item);
+    bool IsFull();
+}
